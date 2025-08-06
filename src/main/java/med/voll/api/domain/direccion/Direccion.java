97e7bee916991c770433e4e7a3bd6de7582c2dd9
Matias <matiasponcexd@gmail.com>
2025-08-06 15:55:07 -0400

    inicializacion del proyecto

--- src/main/java/med/voll/api/domain/direccion/Direccion.java
+++ src/main/java/med/voll/api/domain/direccion/Direccion.java
@@ -1,6 +1,7 @@
 package med.voll.api.domain.direccion;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
